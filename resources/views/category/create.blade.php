ab4796999cfb25d23274c31715f7286b9e420a7f
gauri sarswat <gaurisarswat06@gmail.com>
2024-12-14 21:07:15 +0530

    first commit

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,6 @@
+<h1>This is category form</h1>
+<form action="/category" method="post">
+    @csrf
+    {{-- Cross Site Request Forgery --}}
+    <button>Go</button>
+</form>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function () {
+    return view('user');
+});
+// Route::resource('/category', CategoryController::class);
+Route::get('/category', [CategoryController::class,"index"]);
+Route::get('/category/create', [CategoryController::class,"create"]);
+Route::post('/category', [CategoryController::class,"store"]);
+Route::get('/category/{id}', [CategoryController::class,"show"]);
+Route::get('/category/{id}/edit', [CategoryController::class,"edit"]);
+Route::put('/category/{id}', [CategoryController::class,"update"]);
+Route::delete('/category/{id}', [CategoryController::class,"destroy"]);
